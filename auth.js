a9c73158e9de624d207106916f604958d9826be9
EnvPlayz <82762496+EnvPlayz@users.noreply.github.com>
2022-12-28 19:14:21 +0300

    add logout

--- auth.js
+++ auth.js
@@ -10,3 +10,15 @@ async function isAuthenticated(){
         }
     }).catch(function () { return false })
 }
+function logout(){
+    fetch("https://api.simplebase.ga/auth/logout", {
+        method: 'GET',
+        credentials: "include",
+    }).then((res) => {
+        if (res.status == 200) {
+            _authStat = true
+        } else {
+            _authStat = false
+        }
+    }).catch(function () { return false })
+}
