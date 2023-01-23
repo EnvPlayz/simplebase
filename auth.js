8bdb1a1251532e264bcf68cbdad826023c22b654
EnvPlayz <82762496+EnvPlayz@users.noreply.github.com>
2023-01-23 22:34:31 +0300

    update fixes

--- auth.js
+++ auth.js
@@ -1,4 +1,4 @@
-var __authSt = "not sent yet"
+// var __authSt = "not sent yet"
 document.addEventListener("DOMContentLoaded",()=>{
     if (window.sessionStorage.getItem("served") != JSON.stringify(true)) {
         if (window.location.pathname.includes("/servercheck")) {
@@ -8,25 +8,22 @@ document.addEventListener("DOMContentLoaded",()=>{
     }
 })
 async function isAuthenticated(){
-   if(__authSt!="not sent yet"){
-    return true
-   }else if(__authSt==401){
-    return false;
-   }
     return fetch("https://api.simplebase.ga/auth/user", {
         method: 'GET',
         credentials: "include",
     }).then((res) => {
         if (res.status == 200) {
             res.json().then(function (THEENC) {
-                __authSt = THEENC
+
             })
             return true
-        } else {
+        } else if(res.status==429){
+            window.location.assign("/servercheck.html?proceed=" + window.location.pathname.replace("/", "")+"&reason=429")
+        }else{
             if (res.status != 401) {
                 window.location.assign("/servercheck.html")
             }else{
-                __authSt=401
+
             }
             return false
         }
@@ -35,22 +32,20 @@ async function isAuthenticated(){
 async function authData(){
     const pro = new Promise((resolve, reject) => {
         setTimeout(() => {
-            if (__authSt == "not sent yet") {
-                fetch("https://api.simplebase.ga/auth/user", {
-                    method: 'GET',
-                    credentials: "include",
-                }).then((res) => {
-                    if (res.status == 200) {
-                        res.json().then(function (THEENC) {
-                            resolve(THEENC)
-                        })
-                    } else {
-                        reject(false)
-                    }
-                }).catch(reject)
-            } else {
-                resolve(__authSt)
-            }
+            fetch("https://api.simplebase.ga/auth/user", {
+                method: 'GET',
+                credentials: "include",
+            }).then((res) => {
+                if (res.status == 200) {
+                    res.json().then(function (THEENC) {
+                        resolve(THEENC)
+                    })
+                } else if (res.status == 429) {
+                    window.location.assign("/servercheck.html?proceed=" + window.location.pathname.replace("/", "") + "&reason=429")
+                } else {
+                    reject(false)
+                }
+            }).catch(reject)
         }, 1000);
     });
    return pro
